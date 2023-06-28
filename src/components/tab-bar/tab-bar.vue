4df35b03b646325ecf83a681a51e429f681297cf
2787725910@qq.com <2787725910@qq.com>
2023-06-28 19:44:12 +0800

    抽离tabbar中的数据

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,26 +1,17 @@
 <template>
     <div class="tab-bar">
-        <div class="tab-bar-item">
-            <img src="@/assets/img/tabbar/tab_home.png" alt="">
-            <span class="text">首页</span>
-        </div>
-        <div class="tab-bar-item">
-            <img src="@/assets/img/tabbar/tab_favor.png" alt="">
-            <span class="text">收藏</span>
-        </div>
-        <div class="tab-bar-item">
-            <img src="@/assets/img/tabbar/tab_order.png" alt="">
-            <span class="text">订单</span>
-        </div>
-        <div class="tab-bar-item">
-            <img src="@/assets/img/tabbar/tab_message.png" alt="">
-            <span class="text">信息</span>
-        </div>
+        <template v-for="(item, index) in tabbarData">
+            <div class="tab-bar-item">
+                <img :src="getAssetURL(item.image)" alt="">
+                <span class="text">{{ item.text }}</span>
+            </div>
+        </template>
     </div>
 </template>
 
 <script setup>
-
+import tabbarData from "@/assets/data/tabbar.js"
+import { getAssetURL } from "@/utils/load_assets.js"
 
 </script>
 
