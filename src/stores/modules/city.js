3bc13a4ccfff9be4b71d73c78425fd6c1eb5ff71
2787725910@qq.com <2787725910@qq.com>
2023-06-29 14:41:51 +0800

    city页面通过pinia管理状态

--- src/stores/modules/city.js
+++ src/stores/modules/city.js
@@ -1,10 +1,16 @@
 import { defineStore } from "pinia";
+import { getCityAll } from "@/services";
 
 const useCityStore = defineStore("city", {
     state: () => ({
-        cities: []
+        allCities: {}
     }),
     actions: {
-
+        async fetchAllCitiesData() {
+            const res = await getCityAll()
+            this.allCities = res.data
+        }
     }
 })
+
+export default useCityStore
--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -5,17 +5,18 @@
 
         <!-- tab的切换 -->
         <van-tabs v-model:active="tabActive" color="#ff9854">
-            <van-tab title="国内·港澳台"></van-tab>
-            <van-tab title="海外"></van-tab>
+            <template v-for="(value, key, index) in allCities" :key="key">
+                <van-tab :title="value.title"></van-tab>
+            </template>
         </van-tabs>
-
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
-import { getCityAll } from '@/services'
+import useCityStore from '@/stores/modules/city.js'
+import { storeToRefs } from 'pinia';
 
 const searchValue = ref("")
 const tabActive = ref()
@@ -24,11 +25,24 @@ const router = useRouter()
 const cancelClick = () => {
     router.back()
 }
-
+/* 这个位置发送网络请求有两个缺点
+1.如果网络请求太多，那么页面组件中就包含了大量的对于网络请求和数据处理的逻辑
+2.如果页面封装了很多子组件，子组件需要这些数据，我们必须一步步将数据传递过去
+*/
 // 网络请求： 请求城市的数据
-getCityAll().then(res => {
-    console.log(res);
-})
+// const allCity = ref({})
+// getCityAll().then(res => {
+//     console.log(res);
+//     allCity.value = res.data
+// })
+
+// 从Store中获取数据
+const cityStore = useCityStore()
+cityStore.fetchAllCitiesData()
+// 结构以后allCities就不是响应式的了，通过storeToRefs包裹可以实现响应式
+const { allCities } = storeToRefs(cityStore)
+
+
 
 </script>
 
