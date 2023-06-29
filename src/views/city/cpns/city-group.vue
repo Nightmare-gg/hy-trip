a299899f56220702e80e7a6b4e368854cc8fb9fb
2787725910@qq.com <2787725910@qq.com>
2023-06-29 16:58:20 +0800

    city页面热门部分编写完成

--- src/views/city/cpns/city-group.vue
+++ src/views/city/cpns/city-group.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="city-gruop">
-        <van-index-bar>
+        <van-index-bar :index-list="indexList">
+            <van-index-anchor index="热门" />
+            <div class="list">
+                <template v-for="(city, index) in groupData.hotCities">
+                    <div class="city">{{ city.cityName }}</div>
+                </template>
+            </div>
             <template v-for="(group, index) in groupData.cities" :key="index">
                 <van-index-anchor :index="group.group" />
                 <template v-for="(city, indey) in group.cities" :key="indey">
@@ -12,13 +18,38 @@
 </template>
 
 <script setup>
-defineProps({
+import { computed } from 'vue';
+
+const props = defineProps({
     groupData: {
         type: Object,
         default: () => ({})
     }
 })
 
+const indexList = computed(() => {
+    return props.groupData.cities.map(item => item.group)
+})
+
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.list {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    padding: 10px 20px;
+
+    .city {
+        width: 70px;
+        height: 28px;
+        line-height: 28px;
+        background-color: #fff4ec;
+        border-radius: 14px;
+        margin: 10px 0;
+        text-align: center;
+        font-size: 12px;
+        color: #000;
+    }
+}
+</style>
