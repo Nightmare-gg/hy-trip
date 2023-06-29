411c28cb668e4fb4477d3592a4377892af27b0c0
2787725910@qq.com <2787725910@qq.com>
2023-06-29 13:01:40 +0800

    city页面tab栏的搭建

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="city hiddenTabBar">
+        <!-- 搜索框 -->
         <form action="/">
             <van-search v-model="searchValue" show-action placeholder="城市/区域/位置" shape="round" @cancel="cancelClick" />
         </form>
+
+        <!-- tabs -->
+        <van-tabs v-model:active="tabActive" color="#ff9854">
+            <van-tab title="国内·港澳台"></van-tab>
+            <van-tab title="海外"></van-tab>
+        </van-tabs>
 
     </div>
 </template>
@@ -12,6 +19,7 @@ import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 const searchValue = ref("")
+const tabActive = ref()
 const router = useRouter()
 
 const cancelClick = () => {
