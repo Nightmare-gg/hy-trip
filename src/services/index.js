bec2c9ce34da7b3be868ea4399bee5c218d8a151
2787725910@qq.com <2787725910@qq.com>
2023-06-29 13:55:27 +0800

    city页面网络请求的封装

---
+++ src/services/index.js
@@ -0,0 +1,2 @@
+// 统一导出city中导出的内容
+export * from "./modules/city"
--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,11 +1,9 @@
 <template>
     <div class="city hiddenTabBar">
         <!-- 搜索框 -->
-        <form action="/">
-            <van-search v-model="searchValue" show-action placeholder="城市/区域/位置" shape="round" @cancel="cancelClick" />
-        </form>
+        <van-search v-model="searchValue" show-action placeholder="城市/区域/位置" shape="round" @cancel="cancelClick" />
 
-        <!-- tabs -->
+        <!-- tab的切换 -->
         <van-tabs v-model:active="tabActive" color="#ff9854">
             <van-tab title="国内·港澳台"></van-tab>
             <van-tab title="海外"></van-tab>
@@ -17,6 +15,7 @@
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { getCityAll } from '@/services'
 
 const searchValue = ref("")
 const tabActive = ref()
@@ -25,6 +24,11 @@ const router = useRouter()
 const cancelClick = () => {
     router.back()
 }
+
+// 网络请求： 请求城市的数据
+getCityAll().then(res => {
+    console.log(res);
+})
 
 </script>
 
