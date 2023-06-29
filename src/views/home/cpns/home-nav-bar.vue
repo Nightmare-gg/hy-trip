0d79fcc0cf6e1a2bae6eb179cb8adc3667c61dcb
2787725910@qq.com <2787725910@qq.com>
2023-06-29 10:22:35 +0800

    home-navbar的封装

---
+++ src/views/home/cpns/home-nav-bar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="nav-bar">
+        <div class="title">红云旅途</div>
+    </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style lang="less" scoped>
+.nav-bar {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 46px;
+    border-bottom: 1px solid #f3f3f3;
+
+    .title {
+        font-size: 18px;
+        font-weight: 600;
+        color: var(--primary-color);
+    }
+}
+</style>
