9283780c07fcc7d59b5de946c014ad49ea93b056
2787725910@qq.com <2787725910@qq.com>
2023-06-29 10:43:55 +0800

    home-banner和location编写

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="home">
         <home-nav-bar />
+        <div class="banner">
+            <img src="@/assets/img/home/banner.webp" alt="">
+        </div>
+        <div class="location">
+            <div class="city">广州</div>
+            <div class="position">
+                <div class="text">我的位置</div>
+                <img src="@/assets/img/home/icon_location.png" alt="">
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,4 +19,37 @@ import HomeNavBar from "./cpns/home-nav-bar.vue"
 
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.banner {
+    img {
+        width: 100%;
+    }
+}
+
+.location {
+    display: flex;
+    align-items: center;
+    height: 44px;
+    padding: 0 5px;
+
+    .city {
+        flex: 1;
+    }
+
+    .position {
+        display: flex;
+        align-items: center;
+        width: 74px;
+
+        .text {
+            font-size: 12px;
+        }
+
+        img {
+            width: 18px;
+            height: 18px;
+            margin-left: 5px;
+        }
+    }
+}
+</style>
