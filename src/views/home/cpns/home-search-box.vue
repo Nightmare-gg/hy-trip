170b82325bc0711cf003f6d8a32b1be2e1c85afe
2787725910@qq.com <2787725910@qq.com>
2023-06-29 11:35:34 +0800

    home-search组件的抽离以及获取定位的方式

---
+++ src/views/home/cpns/home-search-box.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="search-box">
+        <div class="location" @click="positionClick">
+            <div class="city">广州</div>
+            <div class="position">
+                <div class="text">我的位置</div>
+                <img src="@/assets/img/home/icon_location.png" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+// 位置/城市
+const positionClick = () => {
+    navigator.geolocation.getCurrentPosition(res => {
+        console.log("获取位置成功：", res);
+    }, err => {
+        console.log("获取位置失败：", err);
+    }, {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    })
+}
+
+</script>
+
+<style lang="less" scoped>
+.location {
+    display: flex;
+    align-items: center;
+    height: 44px;
+    padding: 0 20px;
+
+    .city {
+        flex: 1;
+    }
+
+    .position {
+        display: flex;
+        align-items: center;
+        width: 74px;
+
+        .text {
+            font-size: 12px;
+        }
+
+        img {
+            width: 18px;
+            height: 18px;
+            margin-left: 5px;
+        }
+    }
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -4,52 +4,19 @@
         <div class="banner">
             <img src="@/assets/img/home/banner.webp" alt="">
         </div>
-        <div class="location">
-            <div class="city">广州</div>
-            <div class="position">
-                <div class="text">我的位置</div>
-                <img src="@/assets/img/home/icon_location.png" alt="">
-            </div>
-        </div>
+        <Home-search-box />
     </div>
 </template>
 
 <script setup>
 import HomeNavBar from "./cpns/home-nav-bar.vue"
-
+import HomeSearchBox from "./cpns/home-search-box.vue"
 </script>
 
 <style lang="less" scoped>
 .banner {
     img {
         width: 100%;
-    }
-}
-
-.location {
-    display: flex;
-    align-items: center;
-    height: 44px;
-    padding: 0 5px;
-
-    .city {
-        flex: 1;
-    }
-
-    .position {
-        display: flex;
-        align-items: center;
-        width: 74px;
-
-        .text {
-            font-size: 12px;
-        }
-
-        img {
-            width: 18px;
-            height: 18px;
-            margin-left: 5px;
-        }
     }
 }
 </style>
