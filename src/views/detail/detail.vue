901334e6a27a4fac9556b5f1d19df192beaab03b
2787725910@qq.com <2787725910@qq.com>
2023-07-02 14:26:40 +0800

    详情页landlord组件的封装

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -5,6 +5,7 @@
             <detail-swipe :swipe-data="mainPart.topModule.housePicture.housePics" />
             <detail-infos :top-infos="mainPart.topModule" />
             <detail-facility :house-facility="mainPart.dynamicModule.facilityModule.houseFacility" />
+            <detail-landlord :landlord="mainPart.dynamicModule.landlordModule" />
         </div>
     </div>
 </template>
@@ -17,6 +18,7 @@ import { ref, computed } from 'vue';
 import DetailSwipe from './cpns/detail_01-swipe.vue'
 import DetailInfos from './cpns/detail_02-infos.vue'
 import DetailFacility from './cpns/detail_03-facility.vue'
+import DetailLandlord from './cpns/detail_04-landlord.vue'
 
 const route = useRoute()
 const router = useRouter()
