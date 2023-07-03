d853d355319414c43bb8538e84928294c3ab49ff
2787725910@qq.com <2787725910@qq.com>
2023-07-03 12:01:29 +0800

    详情页-tabControl组件的封装以及监听滚动位置控制它的状态

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,48 +1,64 @@
-import { onMounted, onUnmounted, ref } from "vue"
-import { throttle } from "underscore"
+import { onDeactivated, onMounted, onUnmounted, ref } from 'vue';
+import { throttle } from 'underscore'
 
-// 方法一：
+// console.log(throttle)
+
+
 // export default function useScroll(reachBottomCB) {
-//     const scrollListenerHandler = () => {
-//         const clientHeight = document.documentElement.clientHeight
-//         const scrollTop = document.documentElement.scrollTop
-//         const scrollHeight = document.documentElement.scrollHeight
-//         if (clientHeight + scrollTop >= scrollHeight) {
-//             console.log("滚动到底部了");
-//             if (reachBottomCB) reachBottomCB()
-//         }
+//   const scrollListenerHandler = () => {
+//     const clientHeight = document.documentElement.clientHeight
+//     const scrollTop = document.documentElement.scrollTop
+//     const scrollHeight = document.documentElement.scrollHeight
+//     console.log("-------")
+//     if (clientHeight + scrollTop >= scrollHeight) {
+//       console.log("滚动到底部了")
+//       if (reachBottomCB) reachBottomCB()
 //     }
+//   }
 
-//     onMounted(() => {
-//         window.addEventListener("scroll", scrollListenerHandler)
-//     })
+//   onMounted(() => {
+//     window.addEventListener("scroll", scrollListenerHandler)
+//   })
 
-//     onUnmounted(() => {
-//         window.removeEventListener("scroll", scrollListenerHandler)
-//     })
+//   onUnmounted(() => {
+//     window.removeEventListener("scroll", scrollListenerHandler)
+//   })
 // }
-// 方法二：
-export default function useScroll() {
+
+export default function useScroll(elRef) {
+    let el = window
+
     const isReachBottom = ref(false)
-    const scrollTop = ref(0)
+
     const clientHeight = ref(0)
+    const scrollTop = ref(0)
     const scrollHeight = ref(0)
 
+    // 防抖/节流
     const scrollListenerHandler = throttle(() => {
-        clientHeight.value = document.documentElement.clientHeight
-        scrollTop.value = document.documentElement.scrollTop
-        scrollHeight.value = document.documentElement.scrollHeight
+        if (el === window) {
+            clientHeight.value = document.documentElement.clientHeight
+            scrollTop.value = document.documentElement.scrollTop
+            scrollHeight.value = document.documentElement.scrollHeight
+        } else {
+            clientHeight.value = el.clientHeight
+            scrollTop.value = el.scrollTop
+            scrollHeight.value = el.scrollHeight
+        }
         if (clientHeight.value + scrollTop.value >= scrollHeight.value) {
+            console.log("滚动到底部了")
             isReachBottom.value = true
         }
     }, 100)
+
     onMounted(() => {
-        window.addEventListener("scroll", scrollListenerHandler)
+        if (elRef) el = elRef.value;
+        el.addEventListener("scroll", scrollListenerHandler)
     })
 
     onUnmounted(() => {
-        window.removeEventListener("scroll", scrollListenerHandler)
+        el.removeEventListener("scroll", scrollListenerHandler)
     })
 
-    return { isReachBottom, clientHeight, scrollHeight, scrollTop }
+    return { isReachBottom, clientHeight, scrollTop, scrollHeight }
 }
--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="detail hiddenTabBar">
+    <div class="detail hiddenTabBar" ref="detailRef">
+        <tab-control :titles="['abc', 'cba', 'dgg', 'mnn']" v-if="showTabControl" />
         <van-nav-bar title="房屋详情" left-text="旅途" left-arrow @click-left="onClickLeft" />
         <div class="main" v-if="mainPart">
             <detail-swipe :swipe-data="mainPart.topModule.housePicture.housePics" />
@@ -9,7 +10,13 @@
             <detail-comment :comment="mainPart.dynamicModule.commentModule" />
             <detail-notice :order-rules="mainPart.dynamicModule.rulesModule.orderRules" />
             <detail-map :position="mainPart.dynamicModule.positionModule" />
+            <detail-intro :price-intro="mainPart.introductionModule" />
         </div>
+        <div class="footer">
+            <img src="@/assets/img/detail/icon_ensure.png" alt="">
+            <div class="text">弘源旅途, 永无止境!</div>
+        </div>
+
     </div>
 </template>
 
@@ -18,6 +25,7 @@ import { useRouter, useRoute } from 'vue-router';
 import { getDetailInfos } from '@/services'
 import { ref, computed } from 'vue';
 
+import tabControl from '@/components/tab-control/tab-control.vue'
 import DetailSwipe from './cpns/detail_01-swipe.vue'
 import DetailInfos from './cpns/detail_02-infos.vue'
 import DetailFacility from './cpns/detail_03-facility.vue'
@@ -25,6 +33,9 @@ import DetailLandlord from './cpns/detail_04-landlord.vue'
 import DetailComment from './cpns/detail_05-comment.vue'
 import DetailNotice from './cpns/detail_06-notice.vue'
 import DetailMap from './cpns/detail_07-map.vue'
+import DetailIntro from './cpns/detail_08-intro.vue'
+
+import useScroll from '@/hooks/useScroll'
 
 const route = useRoute()
 const router = useRouter()
@@ -40,7 +51,31 @@ getDetailInfos(houseId).then(res => {
 const onClickLeft = () => {
     router.back()
 }
+// 监听滚动控制tabControl的显示隐藏
+const detailRef = ref()
+const { scrollTop } = useScroll(detailRef)
+const showTabControl = computed(() => {
+    return scrollTop.value > 300
+})
 
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.footer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 120px;
+
+    img {
+        width: 123px;
+    }
+
+    .text {
+        margin-top: 12px;
+        font-size: 12px;
+        color: #7688a7;
+    }
+}
+</style>
